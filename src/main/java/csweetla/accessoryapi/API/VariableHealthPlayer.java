feaa83bad9d4a8f165b035fea6bb7886af93d7af
Colin <colinjswee@gmail.com>
2024-01-14 00:14:39 -0500

    Added ability for players to have variable max health, up to twice as much as vanilla.
    A lot of things are hardcoded for specific cases right now, so fix this in the future.

---
+++ src/main/java/csweetla/accessoryapi/API/VariableHealthPlayer.java
@@ -0,0 +1,8 @@
+package csweetla.accessoryapi.API;
+
+public interface VariableHealthPlayer {
+	int getExtraHP();
+
+	void setExtraHP(int extraHP);
+	void addExtraHP(int extraHP);
+}
--- src/main/java/csweetla/accessoryapi/impl/mixins/EntityPlayerMixin.java
+++ src/main/java/csweetla/accessoryapi/impl/mixins/EntityPlayerMixin.java
@@ -1,16 +1,26 @@
 package csweetla.accessoryapi.impl.mixins;
 
 
+import com.mojang.nbt.CompoundTag;
+import csweetla.accessoryapi.API.HealthHelper;
 import csweetla.accessoryapi.API.TickableWhileWorn;
+import csweetla.accessoryapi.API.VariableHealthPlayer;
+import net.minecraft.core.entity.EntityLiving;
 import net.minecraft.core.entity.player.EntityPlayer;
 import net.minecraft.core.item.ItemStack;
+import net.minecraft.core.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = EntityPlayer.class, remap = false)
-public class EntityPlayerMixin {
+public abstract class EntityPlayerMixin extends EntityLiving implements VariableHealthPlayer {
+
+	public EntityPlayerMixin(World world) {
+		super(world);
+	}
+
 	@Inject(method = "tick", at = @At("TAIL"))
 	public void tick(CallbackInfo ci) {
 		EntityPlayer player = (EntityPlayer) ((Object) this);
@@ -27,4 +37,50 @@ public class EntityPlayerMixin {
 		}
 	}
 
+	// heal extra hearts in peaceful
+	@Inject(method = "onLivingUpdate", at = @At("HEAD"))
+	public void onLivingUpdate(CallbackInfo ci) {
+		if (this.health < 20 || this.world.difficultySetting != 0)
+			return;
+
+		if (this.health < 20 + HealthHelper.getExtraHealth((EntityPlayer) (Object) this) && this.tickCount % 20 * 12 == 0) {
+			this.heal(1);
+		}
+	}
+
+	@Inject(method = "init", at = @At("TAIL"))
+	public void initExtraHP(CallbackInfo ci)
+	{
+		this.entityData.define(31, 0);
+	}
+
+	@Inject(method = "addAdditionalSaveData", at = @At("TAIL"))
+	public void writeExtraHp(CompoundTag tag, CallbackInfo ci) {
+		tag.putInt("ExtraHP", getExtraHP());
+	}
+
+	@Inject(method = "readAdditionalSaveData", at = @At("TAIL"))
+	public void readExtraHp(CompoundTag tag, CallbackInfo ci) {
+		if (tag.containsKey("ExtraHP")) {
+			setExtraHP(tag.getInteger("ExtraHP"));
+		} else {
+			setExtraHP(0);
+		}
+	}
+
+	public int getExtraHP() {
+		return this.entityData.getInt(31);
+	}
+
+	public void setExtraHP(int extraHP) {
+		this.entityData.set(31, extraHP);
+		// if we would have more health that our new max, give us the new max
+		if (health > 20 + extraHP) {
+			health = 20 + extraHP;
+		}
+	}
+
+	public void addExtraHP(int extraHP) {
+		setExtraHP(getExtraHP() + extraHP);
+	}
 }
